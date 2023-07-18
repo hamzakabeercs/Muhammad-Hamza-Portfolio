98419700530bf0eae77c27c94d12c59e0acbcea5
Mian Hamza <hamzakabeercs@gmail.com>
2023-07-18 23:10:06 +0500

    first commit

---
+++ app/javascript/application.js
@@ -0,0 +1,6 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+import "./custom/analytics"
+import "./custom/fontawesome-5-all-5.15.4"
+import "./custom/js-bootstrap.bundle.min"
+
